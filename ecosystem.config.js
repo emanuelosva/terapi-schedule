490407893beb84be61b972510c658c76f1d12a25
emanuelosva <emanuelosva@gmail.com>
2020-10-08 16:21:10 -0500

    Fix: pm2 to nor replication

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,8 @@ module.exports = {
     {
       name: 'api',
       script: 'src/bin/www',
-      instances: 'max',
-      exec_mode: 'cluster',
       ignore_watch: ['[/\\]./', 'node_modules'],
       max_memory_restart: '300M',
-      exp_backoff_restart_delay: 100,
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
@@ -15,7 +12,6 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
       },
-      watch: true,
     },
   ],
 }
