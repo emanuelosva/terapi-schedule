fc4bd88e305fabe01c23cff236cf70a45ae83583
emanuelosva <emanuelosva@gmail.com>
2020-10-08 13:56:01 -0500

    Add: Logger services

--- src/app.js
+++ src/app.js
@@ -5,11 +5,25 @@
  */
 
 const express = require('express')
-
+const { expressLogger } = require('./lib/logger')
 
 /**
  * APP definition
  */
 const app = express()
 
+/**
+ * Logger middleware
+ */
+expressLogger(app)
+
+/**
+ * Parsers
+ */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+/**
+ * App server as module
+ */
 module.exports = { app }
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -3,3 +3,24 @@
  * @fileoverview Logger service.
  * *****************************
  */
+
+const pino = require('pino')
+const morgan = require('morgan')
+
+/**
+ * App Logger - Defaul log level: `ìnfo`
+ */
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' })
+
+/**
+ * Add a logger middleware to log info about request/response
+ * @param {express.Application} app 
+ */
+const expressLogger = (app) => {
+  app.use(morgan('dev'))
+}
+
+module.exports = {
+  logger,
+  expressLogger,
+}
