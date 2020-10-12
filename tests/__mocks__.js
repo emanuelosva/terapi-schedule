a6d090b7e9740677b7d55b6b290523f2aa671699
emanuelosva <emanuelosva@gmail.com>
2020-10-11 20:21:38 -0500

    Add: test for patient endpoints

--- tests/__mocks__.js
+++ tests/__mocks__.js
@@ -3,3 +3,12 @@
  * @fileoverview Tests mocks.
  * **************************
  */
+
+const { nanoid } = require('nanoid')
+
+module.exports.patientMock = {
+  email: `${nanoid()}@mail.com`,
+  name: 'Petter Parker',
+  cel: '+5254298632',
+  zoomId: '910 8957 2648'
+}
---
+++ tests/patient.test.js
@@ -0,0 +1,117 @@
+/**
+ * *******************************
+ * @fileoverview Users e2e testng.
+ * *******************************
+ */
+
+const supertest = require('supertest')
+const { patientMock } = require('./__mocks__')
+const { app } = require('../src/app')
+
+describe('Patients endpoints', () => {
+  const request = supertest(app)
+  const baseUrl = '/api/patients'
+
+  let patientId
+  let cookie
+
+  describe('POST /patients/signup', () => {
+    test('Should return a bad request on invalid body', async (done) => {
+      const response = await request
+        .post(`${baseUrl}/signup`)
+        .set('Accept', 'application/json')
+        .send({})
+
+      const { body, status } = response
+
+      expect(status).toEqual(400)
+      expect(body.error).toBeTruthy()
+      done()
+    })
+
+    test('Should return the patient created', async (done) => {
+      const response = await request
+        .post(`${baseUrl}/signup`)
+        .set('Accept', 'application/json')
+        .send({ ...patientMock })
+
+      const { body, status, headers } = response
+      const cookieSession = headers['set-cookie'][0]
+
+      expect(status).toEqual(201)
+      expect(cookieSession).toBeDefined()
+
+      expect(body._id).toBeDefined()
+      expect(body.email).toEqual(patientMock.email)
+
+      patientId = body._id
+      cookie = cookieSession
+      done()
+    })
+
+    test('Should return a conflict error if existing email', async (done) => {
+      const response = await request
+        .post(`${baseUrl}/signup`)
+        .set('Accept', 'application/json')
+        .send({ ...patientMock })
+
+      const { body, status } = response
+
+      expect(status).toEqual(409)
+      expect(body.error).toBeTruthy()
+      done()
+    })
+  })
+
+  describe('POST /patients/login', () => {
+    test('Should return the user if exists and set cookie', async (done) => {
+      const response = await request
+        .post(`${baseUrl}/login`)
+        .send({ email: patientMock.email })
+
+      const { body, status, headers } = response
+
+      expect(status).toEqual(200)
+      expect(headers['set-cookie'][0]).toBeDefined()
+      expect(body._id).toEqual(patientId)
+      expect(body.email).toEqual(patientMock.email)
+      done()
+    })
+
+    test('Should return not found on unregister user', async (done) => {
+      const response = await request
+        .post(`${baseUrl}/login`)
+        .send({ email: 'invalid@invalidmail.com' })
+
+      const { body, status } = response
+
+      expect(status).toEqual(404)
+      expect(body.error).toBeTruthy()
+      done()
+    })
+  })
+
+  describe('GET /patients', () => {
+    test('Should return the current user info', async (done) => {
+      const response = await request
+        .get(baseUrl)
+        .set('Cookie', cookie)
+
+      const { body, status } = response
+
+      expect(status).toEqual(200)
+      expect(body._id).toEqual(patientId)
+      expect(body.email).toEqual(patientMock.email)
+      done()
+    })
+
+    test('Should return unauthorized error if not cookie', async (done) => {
+      const response = await request.get(baseUrl)
+
+      const { status } = response
+
+      expect(status).toEqual(401)
+      done()
+    })
+  })
+})
